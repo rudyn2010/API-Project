e033f6c02cd93bcb69ad7a32b2a27a58943343b4
Rudy Nguyen <rudyn2010@gmail.com>
2022-07-29 00:06:30 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,4 +28,14 @@ router.post('/', async (req, res, next) => {
     });
 });
 
+//Phase 4: User Logout API Route
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;

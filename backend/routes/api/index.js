25821222980035a042d24150c085f5ba7a1f3e88
Rudy Nguyen <rudyn2010@gmail.com>
2022-08-04 10:23:29 -0700

    Add bookings.js router file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,6 +10,7 @@ const usersRouter = require('./users.js');
 //For future routers spotsRouter('ex file location')
 const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
+const bookingsRouter = require('./bookings.js')
 
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
@@ -23,6 +24,7 @@ router.use('/users', usersRouter);
 //For future imports:
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
+router.user('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });

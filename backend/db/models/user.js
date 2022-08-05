af7f409b2b82a944bbd20173f2b6dd408fe3bc8e
Rudy Nguyen <rudyn2010@gmail.com>
2022-08-05 14:08:25 -0700

    update user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,13 +41,13 @@ module.exports = (sequelize, DataTypes) => {
     };
     static associate(models) {
       // define association here
-      User.hasMany(models.Spot, { foreignKey: 'ownerId' }),
+      User.hasMany(models.Spot, { foreignKey: 'ownerId', onDelete: "CASCADE", hooks: true }),
 
-      User.hasMany(models.Booking, { foreignKey: 'userId' }),
+      User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: "CASCADE", hooks: true }),
 
-      User.hasMany(models.Review, { foreignKey: 'userId' }),
+      User.hasMany(models.Review, { foreignKey: 'userId', onDelete: "CASCADE", hooks: true }),
 
-      User.hasMany(models.Image, { foreignKey: 'userId' })
+      User.hasMany(models.Image, { foreignKey: 'userId', onDelete: "CASCADE", hooks: true })
     }
   }
   User.init({
@@ -102,7 +102,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     scopes: {
       currentUser: {
-        attributes: { exclude: ["hashedPassword"] }
+        attributes: { exclude: ["hashedPassword", "createdAt", "updatedAt"] }
       },
       loginUser: {
         attributes: { exclude: ['createdAt', 'updatedAt']}

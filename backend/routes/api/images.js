3232c344fd39c09750f8ec4874bbc01d2109ddc1
Rudy Nguyen <rudyn2010@gmail.com>
2022-08-05 04:34:22 -0700

    Add Images router

---
+++ backend/routes/api/images.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User, Spot, Image, Booking, Review } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { Op } = require('sequelize');
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,6 +11,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
 const bookingsRouter = require('./bookings.js')
+const imagesRouter = require('./images.js')
 
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
@@ -25,6 +26,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/images', imagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
